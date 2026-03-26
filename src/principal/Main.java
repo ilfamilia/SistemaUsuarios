6d87f0cd2444d4c31ebceb158b49049fa6f0d7da
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-26 11:20:46 -0400

    Crear clase principal para iniciar la ventana de login

---
+++ src/principal/Main.java
@@ -0,0 +1,19 @@
+package principal;
+
+import controlador.ControladorLogin;
+import javax.swing.SwingUtilities;
+import modelo.UsuarioDAO;
+import vista.VentanaLogin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            VentanaLogin vistaLogin = new VentanaLogin();
+            UsuarioDAO usuarioDAO = new UsuarioDAO();
+            ControladorLogin controladorLogin = new ControladorLogin(vistaLogin, usuarioDAO);
+
+            vistaLogin.setVisible(true);
+        });
+    }
+}
