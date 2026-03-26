b8bdf279acf867340ccea88c2e37650031cf283d
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-26 16:04:41 -0400

    Crear controlador principal para cargar usuarios en la tabla

---
+++ src/controlador/ControladorPrincipal.java
@@ -0,0 +1,47 @@
+package controlador;
+
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import modelo.Usuario;
+import modelo.UsuarioDAO;
+import vista.VentanaPrincipal;
+
+public class ControladorPrincipal {
+
+    private VentanaPrincipal vistaPrincipal;
+    private UsuarioDAO usuarioDAO;
+
+    public ControladorPrincipal(VentanaPrincipal vistaPrincipal, UsuarioDAO usuarioDAO) {
+        this.vistaPrincipal = vistaPrincipal;
+        this.usuarioDAO = usuarioDAO;
+
+        cargarUsuariosEnTabla();
+    }
+
+    private void cargarUsuariosEnTabla() {
+        String[] columnas = {"Nombre", "Apellido", "Teléfono", "Correo electrónico", "Usuario"};
+
+        DefaultTableModel modeloTabla = new DefaultTableModel(null, columnas) {
+            @Override
+            public boolean isCellEditable(int fila, int columna) {
+                return false;
+            }
+        };
+
+        List<Usuario> listaUsuarios = usuarioDAO.listarUsuarios();
+
+        for (Usuario usuario : listaUsuarios) {
+            Object[] fila = {
+                usuario.getNombre(),
+                usuario.getApellido(),
+                usuario.getTelefono(),
+                usuario.getCorreoElectronico(),
+                usuario.getUsuario()
+            };
+
+            modeloTabla.addRow(fila);
+        }
+
+        vistaPrincipal.getTablaUsuarios().setModel(modeloTabla);
+    }
+}
