2808860f9f0d16eec6a072cc312c3f7bd2f9312f
Israel Liranzo <45414112+ilfamilia@users.noreply.github.com>
2026-03-26 00:28:29 -0400

    Crear modelo Usuario con atributos y métodos de acceso

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,81 @@
+package modelo;
+
+public class Usuario {
+    
+    private int id;
+    private String usuario;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String correoElectronico;
+    private String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String usuario, String nombre, String apellido, String telefono, String correoElectronico, String contrasena) {
+        this.id = id;
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
